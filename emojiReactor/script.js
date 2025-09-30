cd2e933ac642dcf63f9e579e5de7e1ccf2118d55
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2025-09-30 15:46:55 +0700

    Update script.js

--- emojiReactor/script.js
+++ emojiReactor/script.js
@@ -9,18 +9,10 @@ function updateCount(btn) {
   countEl.textContent = `${currCount}/10`;
 }
 
-const happyBtn = document.querySelector("#happy-btn");
 
-happyBtn.addEventListener("click", () => updateCount(happyBtn));
-
-const confusedBtn = document.querySelector("#confused-btn");
-
-confusedBtn.addEventListener("click", () => updateCount(confusedBtn));
-
-const sadBtn = document.querySelector("#sad-btn");
-
-sadBtn.addEventListener("click", () => updateCount(sadBtn));
-
-const lovingBtn = document.querySelector("#loving-btn");
-
-lovingBtn.addEventListener("click", () => updateCount(lovingBtn));
+const btns = document.querySelectorAll(".emoji-btn");
+btns.forEach ( btn =>{
+  btn.addEventListener("click", ()=>{
+    updateCount(btn);
+  })
+})
